03a3d444baf8e668a15f1d1e3051c196dbd0229e
Otávio <otaviogrocha@yahoo.com.br>
2023-05-22 00:47:38 -0300

    conexao do banco de dados utilizando credenciais do mysql.js com query assincrona

--- src/database/db.js
+++ src/database/db.js
@@ -2,11 +2,15 @@ const mysql = require("mysql2/promise");
 const config = require("../config/mysql");
 
 async function query(sql, params) {
-  /**
-   * @todo create a database connection and perform queries
-   */
+  const connection = await mysql.createConnection(config.db);
+
+  const [ results ] = await connection.execute(sql, params, (err) => {
+    console.log(err);
+  })
+
+  return results;
 }
 
 module.exports = {
   query,
-};
+};ad
