ac7f668eb7f157fa9631fa43ad1f441b5080cf18
Otávio <otaviogrocha@yahoo.com.br>
2023-05-22 01:05:39 -0300

    rotas para acessar o servidor local atraves das requisicoes HTTP + com os cards controllers como callback, e utilizando o express

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const CardsController = require("./app/controllers/CardsController")
 
 const routes = Router();
 
@@ -6,8 +7,11 @@ routes.get("/", (req, res) => {
   res.json({ message: "Hello PUC-MG!" });
 });
 
-/**
- * @todo Create routes
- */
+
+routes.get('/cards', CardsController.list);
+routes.post('/cards', CardsController.create);
+routes.put('/cards/:id', CardsController.update);
+routes.delete('/cards/:id', CardsController.delete);
+routes.get('/cards/:id', CardsController.show);
 
 module.exports = routes;
