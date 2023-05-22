bb1866487c42e28d86baf3a6e2d4caee70b81dd0
Otávio <otaviogrocha@yahoo.com.br>
2023-05-22 00:50:20 -0300

    funcoes assincronas atraves da query de db.js para enviar ao controller das requisições

--- src/database/cards.js
+++ src/database/cards.js
@@ -2,33 +2,52 @@ const db = require("./db");
 const helper = require("./helper");
 
 async function find() {
-  /**
-   * @todo get all cards
-   */
+  const query = `SELECT * FROM cards LIMIT 100`
+
+  const rows = await db.query(query);
+  const data = helper.emptyOrRows(rows);
+
+  return {
+    data
+  }
 }
 
-async function findOne() {
-  /**
-   * @todo get an specific card by id
-   */
+async function findOne(id) {
+  const query = `SELECT * FROM cards WHERE id = ?`;
+  const result = await db.query(query, [id]);
+
+  const data = helper.emptyOrRows(result);
+
+  return {
+    data
+  }
 }
 
-async function create() {
-  /**
-   * @todo create a card
-   */
+async function create( {name, type, desc, atk, def}) {
+  const query = "INSERT INTO cards (`name`, `type`, `desc`, `atk`, `def`) values (?, ?, ?, ?, ?)";
+  const result = await db.query(query, [name, type, desc, atk, def]);
+
+  const message = result.affectedRows ? "Card criado com sucesso." : "Erro ao criar o card";
+
+  return { message };
 }
 
-async function update() {
-  /**
-   * @todo update an specific card
-   */
+async function update(id, { name, type, desc, atk, def}) {
+  const query = "UPDATE cards SET `name` = ?, `type` = ?, `desc` = ?, `atk` = ?, `def` = ? WHERE `id` = ?";
+  const result = await db.query(query, [name, type, desc, atk, def, id])
+
+  const message = result.affectedRows ? "Card atualizado com sucesso" : "Erro ao atualizar card";
+
+  return { message };
 }
 
 async function remove(id) {
-  /**
-   * @todo delete an specific card
-   */
+  const query = "DELETE FROM cards WHERE `id` = ?";
+  const result = await db.query(query, [id]);
+
+  const message = result.affectedRows ? "Card deletado com sucesso" : "Erro ao deletar card!";
+
+  return { message };
 }
 
 module.exports = {
